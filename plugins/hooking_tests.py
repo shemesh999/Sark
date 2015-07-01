27c4b081103dd4d0cb2a9eb2cd83715c43f22f56
tmr232 <tmr232@github>
2015-07-01 17:57:19 +0300

    IDA Tracking

--- plugins/hooking_tests.py
+++ plugins/hooking_tests.py
@@ -1,6 +1,66 @@
+from collections import defaultdict
 import idaapi
-
+import networkx as nx
 import sark
+import sark.qt
+
+
+class IDATracker(idaapi.UI_Hooks):
+    def __init__(self):
+        super(IDATracker, self).__init__()
+
+    def updating_actions(self, ctx=idaapi.action_update_ctx_t()):
+        try:
+            if ctx.form_type == idaapi.BWN_DISASM:
+                self.on_ea(ctx.cur_ea, ctx)
+        except NotImplementedError:
+            pass
+
+    def on_ea(self, ea, ctx):
+        raise NotImplementedError()
+
+    def track(self):
+        self.hook()
+
+
+class LineHighlight(IDATracker):
+    def __init__(self):
+        super(LineHighlight, self).__init__()
+        self.current_ea = None
+
+    def on_ea(self, ea, ctx):
+        if self.current_ea is not None:
+            sark.Line(self.current_ea).color = None
+        sark.Line(ea).color = 0x66EE22
+        if self.current_ea != ea:
+            self.current_ea = ea
+
+
+class UnreachableHighlight(IDATracker):
+    def on_ea(self, ea, ctx):
+        graph = sark.codeblocks.get_nx_graph(ea)
+        my_node = sark.codeblocks.get_block_start(ea)
+        for node in graph.nodes_iter():
+            if nx.has_path(graph, node, my_node) or nx.has_path(graph, my_node, node):
+                sark.codeblocks.get_codeblock(node).color = None
+            else:
+                sark.codeblocks.get_codeblock(node).color = 0x3344DD
+
+
+class FlowTracker(IDATracker):
+    def __init__(self):
+        super(FlowTracker, self).__init__()
+        self.cur_ea = None
+        self.index = 0
+
+    def on_ea(self, ea, ctx):
+        if ea == self.cur_ea:
+            return
+
+        widget = sark.qt.form_to_widget(ctx.form)
+        sark.qt.capture_widget(widget, r"c:\temp\idaflow\idaflow{:08d}.png".format(self.index))
+        self.index += 1
+        self.cur_ea = ea
 
 
 class UiHooks(idaapi.UI_Hooks):
@@ -47,10 +107,13 @@ class HookIDA(idaapi.plugin_t):
     wanted_hotkey = ""
 
     def init(self):
+        idaapi.askaddr(1, "123")
         self.ui_hooks = UiHooks()
         self.ui_hooks.hook()
         self.idb_hooks = IDBHooks()
         self.idb_hooks.hook()
+        self.flow = FlowTracker()
+        self.flow.track()
         return idaapi.PLUGIN_KEEP
 
     def term(self):
